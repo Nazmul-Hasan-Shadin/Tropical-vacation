15edbbd834ea0b20e3ff04715e4c53b8cefa25f1
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-12-17 11:32:22 +0600

    advanture section data has migrated to json

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -3,15 +3,15 @@ import profile1 from '../../assets/images/femaleavatar1.png'
 import profile2 from '../../assets/images/femaleavatar2.png'
 import profile3 from '../../assets/images/femaleavatar3.png'
 import './Card.css'
-const Card = ({image,text}) => {
+const Card = ({image,text,data}) => {
     return (
         <div className='card-container'>
          <section className='card'>
             
-            <img src={profile1} alt="" />
+            <img src={data?.image} alt="" />
              <div className='card-text'>
-                <h3 className='name'>{'Nazmul Hasn Shadin'}</h3>
-                <p className='desc'>{'text lorem12 dkjfk kfjdk kdfjd kdfjkd fkfjkd'}</p>
+                <h3 className='name'>{data?.name}</h3>
+                <p className='desc'>{data?.desc}</p>
              
              </div>
             </section>     
--- src/Component/TropicalAdvancture/TropicalAdvancture.jsx
+++ src/Component/TropicalAdvancture/TropicalAdvancture.jsx
@@ -1,15 +1,31 @@
 
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import './Advanture.css'
 
 const TropicalAdvancture = () => {
+  const [cardData,setCardData]=useState([])
+
+
+ 
+    useEffect(()=>{
+      fetch('cardData.json')
+      .then(res=>res.json())
+      .then(data=> setCardData(data))
+    },[])
+ console.log(cardData);
+
+  
   return (
   
       <section className="wrapper">
         <div className="card-containerr">
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
+      
+  {
+    cardData.map(data=> <Card data={data} ></Card>)
+  }
+
+
         </div>
         {/* adventure text */}
         <div className="advanture-text">
@@ -17,11 +33,11 @@ const TropicalAdvancture = () => {
           <p>Student Tropical Vacation: Relax and Recharge</p>
            
             <ul>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet</li>
+                <li>Experiential Learning in Breathtaking Landscapes</li>
+                <li>Exclusive Student-Friendly Discounts and Offers</li>
+                <li>Educational Excursions blending Learning and Exploration</li>
+                <li>Thrilling Adventure Sports and Activities</li>
+                <li>Community Building with Like-Minded Students</li>
             </ul>
             <button>Explore more</button>
         </div> 
